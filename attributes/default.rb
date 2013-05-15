b66e68c394004a6f130a5f63010915b2ddab2526
Matt Whiteley <mattwhiteley@gmail.com>
2013-05-15 00:44:15 -0600

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,6 @@
 default['vagrant']['base_url'] = 'http://files.vagrantup.com/packages'
 default['vagrant']['checksum'] = '1581552841e076043308f330a5b1130b455c604846116c54b5330bb17240c7ee'
 default['vagrant']['url_hash'] = '7e400d00a3c5a0fdf2809c8b5001a035415a607b'
-
-default['vagrant']['version'] = '1.2.2'
+default['vagrant']['version']  = '1.2.2'
 default['vagrant']['dmg_name'] = "Vagrant-#{node['vagrant']['version']}.dmg"
-default['vagrant']['source'] = "#{node['vagrant']['base_url']}/#{node['vagrant']['url_hash']}/#{node['vagrant']['dmg_name']}"
+default['vagrant']['source']   = "#{node['vagrant']['base_url']}/#{node['vagrant']['url_hash']}/#{node['vagrant']['dmg_name']}"
